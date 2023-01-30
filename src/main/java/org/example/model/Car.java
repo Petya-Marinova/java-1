e9ca53d7d727d7da41d36e12c263dad8a951d175
Petya Marinova <petya.marinova@abv.bg>
2023-01-30 20:17:06 +0200

    init commit

---
+++ src/main/java/org/example/model/Car.java
@@ -0,0 +1,22 @@
+package org.example.model;
+
+import lombok.*;
+
+import java.util.Objects;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Data
+public class Car {
+    private String brand;
+    private String model;
+    private String engineVolume;
+
+
+
+
+
+
+}
