15380f13623ec776e11a5175be933a46a10f8006
Petya Marinova <petya.marinova@abv.bg>
2023-01-30 20:40:33 +0200

    getters and setters added

--- src/main/java/org/example/model/SportCar.java
+++ src/main/java/org/example/model/SportCar.java
@@ -1,2 +1,15 @@
-package org.example.model;public class SportCar {
+package org.example.model;
+
+public class SportCar extends Car{
+
+    private int spoiler;
+
+
+    public int getSpoiler() {
+        return spoiler;
+    }
+
+    public void setSpoiler(int spoiler) {
+        this.spoiler = spoiler;
+    }
 }
